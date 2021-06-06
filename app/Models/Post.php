82e97e6392d80e1d89a3394f6581189cccc09e6f
glen-s-abraham <glenprojects666@gmail.com>
2021-06-06 13:51:36 +0530

    Completed Migration files, Models, Relations And Factories

---
+++ app/Models/Post.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Image;
+use App\Models\Comment;
+use App\Models\Like;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded=[];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function image()
+    {
+        return $this->morphOne(Image::class,name:'imagable');
+    }
+    public function comments()
+    {
+        return $this->morphMany(Comment::class,name:'commentable');
+    }
+    public function likes()
+    {
+        return $this->morphMany(Like::class,name:'likable');
+    }
+
+
+}

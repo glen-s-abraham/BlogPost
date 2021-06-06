d3c6cf2781486da9301e06add06c7ea75699c89c
glen-s-abraham <glenprojects666@gmail.com>
2021-06-06 17:52:59 +0530

    Controllers and Endpoint Outlines included

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Users
+Route::resource('user','User\UserController')
+->only(['store','show','update','destroy']);
+
+//Posts
+Route::resource('posts','Post\PostController')
+->only(['index','store','show','update','destroy']);
+
+//Comments
+
+Route::prefix('comments')->group(function(){
+
+    Route::get('/post-comments/{id}','Comment\CommentController@getPostComment')->name('post.comments');
+    Route::get('/comment-replies/{id}','Comment\CommentController@getCommentReplies')->name('comment.replies');
+    Route::post('/post-comment/{id}','Comment\CommentController@postComment')->name('post.comment.store');
+    Route::post('/reply-comment/{id}','Comment\CommentController@replyComment')->name('reply.comment.store');
+    Route::post('/edit-comment/{id}','Comment\CommentController@update')->name('comment.edit');
+    Route::post('/destroy-comment/{id}','Comment\CommentController@destroy')->name('comment.delete');
 });
+
+//likes
+Route::prefix('likes')->group(function(){
+    Route::get('/post-likes/{id}','Like\LikeController@getPostLikes')->name('post.likes');
+    Route::get('/comment-likes/{id}','Like\LikeController@getCommentLikes')->name('comment.likes');
+    Route::post('/like-post/{id}','Like\LikeController@postLike')->name('post.like.store');
+    Route::post('/like-comment/{id}','Like\LikeController@commentLike')->name('comment.like.store');
+    Route::post('/unlike-post/{id}','Like\LikeController@postUnike')->name('post.like.destroy');
+    Route::post('/unlike-comment/{id}','Like\LikeController@commentUnike')->name('comment.like.destroy');
+});
+    

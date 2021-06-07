ab0cf05ce4f8683d882ac1d47abe13d35ff3dd2b
glen-s-abraham <glenprojects666@gmail.com>
2021-06-07 17:52:40 +0530

    User Controller Logic added without authentication logic

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,16 +2,30 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserStoreRequest;
+use App\Http\Requests\UserUpdateRequest;
 
-class UserController extends Controller
+
+class UserController extends ApiController
 {
     
-    public function store(Request $request)
+    public function store(UserStoreRequest $request)
     {
-        //
+        $data=$request->all();
+        $data['password']=bcrypt($request->password);
+        $user=User::create($data);
+        if($request->hasFile('image'))
+        {
+            $image=$request->file('image')->store('public/profiles');
+            $user->image()->create(['url'=>$image]);
+        }
+      
+        
+
+        return $this->showOne($user,201);
     }
 
     /**
@@ -22,7 +36,9 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        //
+        //To be replaced with currently authenticated user
+        
+        return $this->showOne($user);
     }
 
     /**
@@ -31,9 +47,41 @@ class UserController extends Controller
      * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, User $user)
+    public function update(UserUpdateRequest $request, User $user)
     {
-        //
+        //Should Replace user instance with logged in user
+
+        if($request->has('name')){
+            $user->name=$request->name;
+        }
+
+        if($request->has('email') && $user->email != $request->email){
+            $user->email=$request->email;
+
+        }
+
+        if($request->has('password')){
+            $password=bcrypt($request->password);
+            $user->password=$password;   
+            
+        }
+
+        //doubtful regarding the update of files
+        if($request->hasFile('image'))
+        {
+            $image=$request->file('image')->store('public/profiles');
+            $user->image()->updateOrCreate(['url'=>$image]);
+        }
+
+        if(!$user->isDirty())
+        {
+
+            return $this->errorResponse("You need to specify a different value to update",422);
+        }
+
+        $user->save();
+
+        return $this->showOne($user);
     }
 
     /**
@@ -44,6 +92,10 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+
+        //Should Replace user instance with logged in user
+
+        $user->delete();
+        return $this->showOne($user);
     }
 }
